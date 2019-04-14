88799613a4bec2ad76ff01ea44c169b42ea26cf4
Sven Anders Robbestad <sven@robbestad.com>
2019-04-14 11:09:58 +0200

    Init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Iframe from "react-iframe";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+<Iframe url="http://www.youtube.com/embed/xDMP3i36naA"
+        width="600px"
+        height="600px"
+        id="myId"
+        className="myClassname"
+        display="initial"
+        position="relative"
+        allowFullScreen/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
